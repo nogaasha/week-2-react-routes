2c66ad5f25269cc46ad93ecd0894c3f78640d944
Victoria <sivosha2006@mail.u>
2020-06-01 21:31:34 +0300

    change file

--- client/components/dashboard.js
+++ client/components/dashboard.js
@@ -2,20 +2,22 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Head from './head'
 
-const Dummy = () => {
+const Dashboard = () => {
   return (
     <div>
       <Head title="Hello" />
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          This is dummy component
-          <Link to="/dashboard"> Go To Dashboard </Link>
+          <div id="title">Dashboard</div>
+          This is Dashboard
+          <Link to="/dashboard/profile/bf20e70d-e50c-4bf9-8092-69c044c07b08"> Go To Profile </Link>
+          <Link to="/dashboard/main"> Go To Main </Link>
         </div>
       </div>
     </div>
   )
 }
 
-Dummy.propTypes = {}
+Dashboard.propTypes = {}
 
-export default Dummy
+export default Dashboard
--- client/components/home.js
+++ client/components/home.js
@@ -1,4 +1,6 @@
+/* eslint-disable react/require-default-props */
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Header from './header'
 
 const Home = () => {
@@ -7,7 +9,7 @@ const Home = () => {
       <Header />
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          {/* add your routes here */}
+          <Link to="/dashboard"> Go To Dashboard </Link>
         </div>
       </div>
     </div>
