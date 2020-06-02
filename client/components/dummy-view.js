aa7a837e7c40b28a1bdb0b275068524cdd86e7f2
Nogaasha <sivosha2010@gmail.com>
2020-06-02 20:51:17 +0300

    changes in client

--- client/components/dummy-view.js
+++ client/components/dummy-view.js
@@ -5,11 +5,11 @@ import Head from './head'
 const Dummy = () => {
   return (
     <div>
-      <Head title="Hello" />
+      <Head title="Hellodw" />
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
           This is dummy component
-          <Link to="/dashboard"> Go To Dashboard </Link>
+          <Link to="/dashboard/"> Go To Dashboard </Link>
         </div>
       </div>
     </div>
--- client/components/home.js
+++ client/components/home.js
@@ -1,13 +1,25 @@
 /* eslint-disable react/require-default-props */
 import React from 'react'
+import { Link, Switch, Route } from 'react-router-dom'
 import Header from './header'
+import Main from './dashboard-main'
+import Profile from './dashboard-profile'
+import Dashboard from './dashboard'
 
 const Home = () => {
   return (
     <div>
       <Header />
       <div className="flex items-center justify-center h-screen">
-        <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10" />
+        <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
+          Hello World Dashboard
+          <Switch>
+            <Route exact path="/dashboard/" component={() => <Dashboard />} />
+            <Route exact path="/dashboard/main" component={() => <Main />} />
+            <Route exact path="/dashboard/profile/:user" component={() => <Profile />} />
+          </Switch>  
+          <Link to="/"> Go To Root </Link>
+        </div>
       </div>
     </div>
   )
